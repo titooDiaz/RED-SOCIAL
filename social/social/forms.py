5734f209ddab5cd99c6724273f01818621306c19
tito <107213851+migueltitoxd@users.noreply.github.com>
2023-08-26 14:07:45 -0500

    arreglos

--- social/social/forms.py
+++ social/social/forms.py
@@ -11,11 +11,10 @@ class SocialPostForm(forms.ModelForm):
         required=True)
 
     image = forms.FileField(widget=forms.ClearableFileInput(attrs={
+        'multiple': False,
         'class': 'relative dark:text-dark-txt dark:bg-dark-second cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500',
-        'multiple': True
         }),
-        required=False
-        )
+        required=False)
     
     widgets = {
             'body': forms.Textarea(attrs={'class': 'rounded-md bg-green-100 border p-2 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 dark:bg-dark-third dark:border-dark-third dark:text-dark-txt flex max-w-full sm:text-sm border-gray-300 rounded-md mx-auto'}),
